b085c3cc175f8f1f9f75a3701589a6d5cca042d7
musense <musense.ad@gmail.com>
2023-11-29 14:57:47 +0800

    first commit

--- src/app/__tests__/Home.test.tsx
+++ src/app/__tests__/Home.test.tsx
@@ -3,49 +3,45 @@ import userEvent from '@testing-library/user-event'
 import Home from '../page'
 
 describe('Home', () => {
+  it('should add a new todo', async () => {
+    render(<Home />) // ARRANGE
 
-    it('should add a new todo', async () => {
-        render(<Home />) // ARRANGE
+    // ACT
+    const input = screen.getByPlaceholderText('New Todo')
+    await userEvent.type(input, 'My new todo')
+    expect(input).toHaveValue('My new todo') // ASSERT
 
-        // ACT
-        const input = screen.getByPlaceholderText('New Todo')
-        await userEvent.type(input, 'My new todo')
-        expect(input).toHaveValue('My new todo') // ASSERT
-
-        // ACT
-        const button = screen.getByRole('button', {
-            name: 'Submit'
-        })
-        await userEvent.click(button)
-        expect(input).toHaveValue("") // ASSERT 
-
-        const data = await screen.findByText('My new todo')
-        expect(data).toHaveTextContent('My new todo')
+    // ACT
+    const button = screen.getByRole('button', {
+      name: 'Submit'
     })
+    await userEvent.click(button)
+    expect(input).toHaveValue('') // ASSERT
 
-    it('should update a todo', async () => {
-        render(<Home />) // ARRANGE
+    const data = await screen.findByText('My new todo')
+    expect(data).toHaveTextContent('My new todo')
+  })
 
-        // ACT
-        const checkbox = screen.getAllByRole('checkbox')[0] as HTMLInputElement
-        expect(checkbox.checked).toBeFalsy()
-        await userEvent.click(checkbox)
-        expect(checkbox.checked).toBeTruthy() // ASSERT 
+  it('should update a todo', async () => {
+    render(<Home />) // ARRANGE
 
-    })
+    // ACT
+    const checkbox = screen.getAllByRole('checkbox')[0] as HTMLInputElement
+    expect(checkbox).not.toBeChecked()
+    await userEvent.click(checkbox)
+    expect(checkbox).toBeChecked() // ASSERT
+  })
 
-    it('should delete a todo', async () => {
-        render(<Home />) // ARRANGE
+  it('should delete a todo', async () => {
+    render(<Home />) // ARRANGE
 
-        const todoText = screen.queryByText('Write Code 💻')
-        expect(todoText).toBeInTheDocument() // ASSERT 
+    const todoText = screen.queryByText('Write Code 💻')
+    expect(todoText).toBeInTheDocument() // ASSERT
 
-        // ACT
-        const button = screen.getAllByTestId('delete-button')[0]
-        await userEvent.click(button)
+    // ACT
+    const button = screen.getAllByTestId('delete-button')[0]
+    await userEvent.click(button)
 
-        expect(todoText).not.toBeInTheDocument() // ASSERT 
-
-    })
-
+    expect(todoText).not.toBeInTheDocument() // ASSERT
+  })
 })
